8b64680e64c3879e0043e34bf0039d9841647c0b
carly <carly.cheung@solutionforest.net>
2023-11-10 10:32:37 +0800

    remove theme.css after update new version of filament-tree

--- src/app/Providers/Filament/AdminPanelProvider.php
+++ src/app/Providers/Filament/AdminPanelProvider.php
@@ -67,8 +67,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->viteTheme('resources/css/filament/admin/theme.css');
+            ]);
 
         $this->renderHook($panel);
 
